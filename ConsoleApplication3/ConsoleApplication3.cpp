777ea796b72c8e94b6214d97204e2c6b9939dbb4
Nadeeshani <Nadeeshani@NADEESHANI>
2016-04-12 10:37:50 +0530

    Add project files.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello World ";
+	return 0;
+}
+
